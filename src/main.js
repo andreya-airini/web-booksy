55fa7b8b21b1f7b1a0969e0ef42c8f8c2537d11c
Andreya Airini <airiniandreia279@gmail.com>
2025-07-04 02:20:35 +0300

    update template

---
+++ src/main.js
@@ -0,0 +1,10 @@
+import './js/header';
+import './js/mobile-menu';
+import './js/hero';
+import './js/book';
+import './js/book-modal';
+import './js/feedback';
+import './js/events';
+import './js/contact-modal';
+
+
