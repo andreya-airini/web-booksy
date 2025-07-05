5dd078261038bd379a39a2cae0ddff9b60d4216b
Eliza <elizavetasmolka@gmail.com>
2025-07-06 01:48:39 +0200

    Add events section

--- src/js/events.js
+++ src/js/events.js
@@ -1,2 +1,29 @@
 import Swiper from 'swiper';
-import 'swiper/swiper-bundle.css'; 
+import 'swiper/swiper-bundle.css';
+
+
+
+document.addEventListener('DOMContentLoaded', () => {
+  const swiper = new Swiper('.swiper-container', {
+    slidesPerView: 1,
+    spaceBetween: 24,
+    navigation: {
+      nextEl: '.slider-controls .next',
+      prevEl: '.slider-controls .prev',
+    },
+    pagination: {
+      el: '.swiper-pagination',
+      clickable: true,
+    },
+    breakpoints: {
+      768: {
+        slidesPerView: 2,
+      },
+      1024: {
+        slidesPerView: 3,
+      },
+    },
+  }
+  ); console.log(document.querySelector('.swiper-container'));
+});
+
