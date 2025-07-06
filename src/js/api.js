a87577ff8e6df3b8be189894e61fb37d295327d0
Olena Hryhorzhevska <elenahry8@gmail.com>
2025-07-06 10:11:07 -0400

    books section is done

--- src/js/api.js
+++ src/js/api.js
@@ -1,1 +1,36 @@
 import axios from 'axios';
+const API_BASE_URL = 'https://books-backend.p.goit.global/books';
+
+export async function fetchTopBooks() {
+  try {
+    const response = await axios.get(
+      `${API_BASE_URL}/top-books`
+    );
+    const topBooks = response.data.flatMap(category => category.books);
+    const uniqueBooks = topBooks.filter(
+      (book, index, self) =>
+        index ===
+        self.findIndex(b => b.title === book.title && b.author === book.author)
+    );
+    return uniqueBooks;
+    
+  } catch (error) {
+    console.error('Error fetching top books:', error);
+  }
+}
+
+export async function fetchBooksByCategory(category) {
+  try {
+    const response = await axios.get(
+      `${API_BASE_URL}/category?category=${encodeURIComponent(category)}`
+    );
+    const uniqueBooks = response.data.filter(
+      (book, index, self) =>
+        index ===
+        self.findIndex(b => b.title === book.title && b.author === book.author)
+    );
+    return uniqueBooks;
+  } catch (error) {
+    console.error('Error fetching books by category:', error);
+  }
+}
---
+++ src/js/book.js
@@ -0,0 +1,157 @@
+import { fetchTopBooks, fetchBooksByCategory } from './api.js';
+
+const categories = document.querySelectorAll('.category-item');
+const categoriesSelect = document.querySelector('.category-select');
+const booksContainer = document.querySelector('.books');
+const loader = document.querySelector('.loader');
+const showMore = document.querySelector('.show-more-btn');
+const booksCount = document.querySelector('#books-count');
+
+function showLoader() {
+  loader.classList.remove('hidden');
+}
+
+function hideLoader() {
+  loader.classList.add('hidden');
+}
+
+function showShowMoreBtn() {
+  showMore.classList.remove('hidden');
+}
+
+function hideShowMoreBtn() {
+  showMore.classList.add('hidden');
+}
+
+function capitalizeTitle(title) {
+  return title
+    .toLowerCase()
+    .split(' ')
+    .map(word => word.charAt(0).toUpperCase() + word.slice(1))
+    .join(' ');
+}
+
+let allBooks = [];
+let currentIndex = 0;
+let initialAmount = window.innerWidth < 768 ? 10 : 24;
+const stepRender = 4;
+
+function renderBatch(amount) {
+  const rendered = Math.min(currentIndex + amount, allBooks.length);
+  const nextSlice = allBooks.slice(currentIndex, rendered);
+  booksContainer.insertAdjacentHTML('beforeend', createMarkup(nextSlice));
+  currentIndex = rendered;
+  booksCount.textContent = `Showing ${rendered} of ${allBooks.length}`;
+  if (currentIndex >= allBooks.length) {
+    hideShowMoreBtn();
+  }
+}
+
+hideShowMoreBtn();
+showLoader();
+fetchTopBooks()
+  .then(books => {
+    allBooks = books;
+    renderBatch(initialAmount);
+    if (allBooks.length > currentIndex) {
+      showShowMoreBtn();
+    }
+  })
+  .finally(() => {
+    hideLoader();
+  });
+
+
+showMore.addEventListener('click', () => {
+  renderBatch(stepRender);
+  const firstCard = document.querySelector('.books li');
+  const cardHeight = firstCard.getBoundingClientRect().height;
+  window.scrollBy({
+    top: cardHeight * 1,
+    behavior: 'smooth',
+  });
+  showMore.blur();
+});
+
+
+// booksContainer.addEventListener('click', e => {
+//   if (e.target.classList.contains('btn')) {
+//     e.target.blur();
+//   }
+// });
+
+function createMarkup(books) {
+  return books
+    .map(item => {
+      return `
+<li class="book-item">
+  <img class="item-image" src="${item.book_image}" alt="${item.title}" />
+  
+  <div class="book-content">
+    <div class="book-info">
+      <div class="text-group">
+        <h3 class="item-title">${capitalizeTitle(item.title)}</h3>
+        <p class="item-author">${item.author}</p>
+      </div>
+      <p class="item-price">$${item.price}</p>
+    </div>
+  </div>
+
+  <button class="btn">Learn More</button>
+</li>`;
+    })
+    .join('');
+}
+
+categoriesSelect.addEventListener('change', () => {
+  const categoryName = categoriesSelect.value;
+  hideShowMoreBtn();
+  showLoader();
+  booksContainer.innerHTML = '';
+  currentIndex = 0;
+
+  const fetchFn =
+    categoryName === 'All categories'
+      ? fetchTopBooks
+      : () => fetchBooksByCategory(categoryName);
+
+  fetchFn()
+    .then(books => {
+      allBooks = books;
+      renderBatch(initialAmount);
+      if (allBooks.length > currentIndex) {
+        showShowMoreBtn();
+      }
+    })
+    .finally(() => {
+      hideLoader();
+    });
+});
+
+categories.forEach(category => {
+  category.addEventListener('click', () => {
+    const categoryName = category.dataset.category;
+
+    hideShowMoreBtn();
+    showLoader();
+    booksContainer.innerHTML = '';
+    currentIndex = 0;
+
+    const fetchFn =
+      categoryName === 'All categories'
+        ? fetchTopBooks
+        : () => fetchBooksByCategory(categoryName);
+
+    fetchFn()
+      .then(books => {
+        allBooks = books;
+        renderBatch(initialAmount);
+        if (allBooks.length > currentIndex) {
+          showShowMoreBtn();
+        }
+      })
+      .finally(() => {
+        hideLoader();
+      });
+  });
+});
